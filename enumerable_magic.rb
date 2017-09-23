6c73cbcd790e192e8ca95baeeb104d647c6a75ac
Isil Donmez <isil.donmez@gmail.com>
2017-09-23 14:04:18 +0100

    Add some notes

--- enumerable_magic.rb
+++ enumerable_magic.rb
@@ -13,3 +13,17 @@ end
 def find_3(list, &block)
   list.detect(&block)
 end
+
+# `detect` and `find` are two names for the same method.
+
+# find(ifnone = nil) { |obj| block } → obj or nil
+# Passes each entry in enum to block.
+# Returns the first for which block is not false.
+# If no object matches, calls ifnone and returns its result when it is specified,
+# or returns nil otherwise.
+
+# find(ifnone = nil) → an_enumerator
+# If no block is given, an enumerator is returned instead.
+
+# To check find_index and find_all methods,
+# please visit https://ruby-doc.org/core-2.2.3/Enumerable.html#method-i-find
